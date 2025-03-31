5b3aa11221f81c138a5c9298abd9464fc4526833
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-04-01 01:56:55 +0300

    Made map instead list and added i18n and l10n for fields

--- passenger-service/src/main/java/com/java/akdev/passengerservice/handler/ValidationExceptionHandler.java
+++ passenger-service/src/main/java/com/java/akdev/passengerservice/handler/ValidationExceptionHandler.java
@@ -5,17 +5,20 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice(basePackages = "com.java.akdev.passengerservice.controller")
 @RequiredArgsConstructor
 public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String PREFIX = "PassengerController.field.";
 
     private final MessageSource messageSource;
 
@@ -24,10 +27,12 @@ public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers,
                                                                   HttpStatusCode status,
                                                                   WebRequest request) {
-        List<String> errors = new ArrayList<>();
+        Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            errors.add(messageSource.getMessage(errorMessage, null, request.getLocale()));
+            errors.put(messageSource.getMessage(PREFIX + fieldName, null, request.getLocale()),
+                    messageSource.getMessage(errorMessage, null, request.getLocale()));
         });
         return ResponseEntity.status(400).body(errors);
     }
