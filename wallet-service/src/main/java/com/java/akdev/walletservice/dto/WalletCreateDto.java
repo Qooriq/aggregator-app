163f4dca1985c8c670439a79d6d381becb48ffea
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-04-04 14:56:04 +0300

    fixed validations

--- wallet-service/src/main/java/com/java/akdev/walletservice/dto/WalletCreateDto.java
+++ wallet-service/src/main/java/com/java/akdev/walletservice/dto/WalletCreateDto.java
@@ -1,13 +1,17 @@
 package com.java.akdev.walletservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
 public record WalletCreateDto(
+        @NotBlank
         String cardNumber,
+        @NotBlank
         Double amount,
+        @NotBlank
         UUID userId
 ) {
 }
--- wallet-service/src/main/java/com/java/akdev/walletservice/dto/WalletReadDto.java
+++ wallet-service/src/main/java/com/java/akdev/walletservice/dto/WalletReadDto.java
@@ -1,13 +1,10 @@
 package com.java.akdev.walletservice.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record WalletReadDto(
-        @NotBlank
         String cardNumber,
-        @NotBlank
         Double amount
 ) {
 }
