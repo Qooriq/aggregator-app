1c850192fe9354248583bc4f376ab178c0a1bf51
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-04-05 16:06:54 +0300

    added validation

--- rides-service/src/main/java/com/java/akdev/ridesservice/dto/RideUpdateDto.java
+++ rides-service/src/main/java/com/java/akdev/ridesservice/dto/RideUpdateDto.java
@@ -1,6 +1,7 @@
 package com.java.akdev.ridesservice.dto;
 
 import com.java.akdev.ridesservice.enumeration.RideStatus;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 import java.time.Instant;
@@ -8,14 +9,21 @@ import java.util.UUID;
 
 @Builder
 public record RideUpdateDto(
+        @NotBlank
         UUID passengerId,
+        @NotBlank
         UUID driverId,
         Long passengerReviewDriver,
         Long driverReviewPassenger,
+        @NotBlank
         String startLocation,
+        @NotBlank
         String endLocation,
+        @NotBlank
         Instant startTime,
+        @NotBlank
         Instant endTime,
+        @NotBlank
         Double ridePrice,
         RideStatus rideStatus
 ) {
