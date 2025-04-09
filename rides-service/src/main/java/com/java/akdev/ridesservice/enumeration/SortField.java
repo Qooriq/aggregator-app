7c3b177b1a359b7d41318116802ca4275fa90216
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-04-09 12:23:08 +0300

    fixed exception handler

--- rides-service/src/main/java/com/java/akdev/ridesservice/enumeration/SortField.java
+++ rides-service/src/main/java/com/java/akdev/ridesservice/enumeration/SortField.java
@@ -7,9 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SortField {
     ID("id"),
-    CARD_NUMBER("cardNumber"),
-    AMOUNT("amount"),
-    USER_ID("userId");
+    PASSENGER_ID("passengerId"),
+    DRiVER_ID("driverId"),
+    DRIVER_REVIEW_PASSENGER("userId"),
+    PASSENGER_REVIEW_DRIVER("passengerReviewDriver"),
+    START_LOCATION("startLocation"),
+    END_LOCATION("endLocation"),
+    START_TIME("startTime"),
+    END_TIME("endTime"),
+    RIDE_PRICE("ridePrice");
 
     private final String name;
 }
--- rides-service/src/main/java/com/java/akdev/ridesservice/handler/ValidationExceptionHandler.java
+++ rides-service/src/main/java/com/java/akdev/ridesservice/handler/ValidationExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.java.akdev.ridesservice.handler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.java.akdev.ridesservice.dto.ErrorResponse;
 import com.java.akdev.ridesservice.exception.RideNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -7,21 +8,24 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 @ControllerAdvice
 @RequiredArgsConstructor
 public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final String PREFIX = "argumentNotValid.field";
+    private static final String PREFIX = "argumentNotValid.field.";
     private final MessageSource messageSource;
 
     @Override
@@ -38,8 +42,30 @@ public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(400).body(errors);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        var cause = ex.getCause();
+        if (cause instanceof InvalidFormatException invalidFormatException) {
+            String fieldName = invalidFormatException.getPath().get(0).getFieldName();
+
+            errors.put(PREFIX + fieldName, fieldName + ".error");
+        }
+        return ResponseEntity.status(400).body(errors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getParameterValidationResults().forEach(error -> {
+            Object fieldName = error.getMethodParameter().getParameterName();
+            errors.put(PREFIX + fieldName,
+                    PREFIX + fieldName + ".mustBePositive");
+        });
+        return ResponseEntity.status(400).body(errors);    }
+
     @ExceptionHandler(RideNotFoundException.class)
     public ResponseEntity<?> handleRideNotFound(RideNotFoundException e) {
-        return ResponseEntity.status(404).body(new ErrorResponse("rideNotFound.error"));
+        return ResponseEntity.status(404).body(new ErrorResponse(e.getMessage()));
     }
 }
