ffb1e72017be324d07af30f7e77f489f4ee12617
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-04-21 03:41:12 +0300

    added synchronized communication between wallets and rides

---
+++ rides-service/src/main/java/com/java/akdev/ridesservice/client/WalletFeignClient.java
@@ -0,0 +1,16 @@
+package com.java.akdev.ridesservice.client;
+
+import com.java.akdev.ridesservice.dto.WalletResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.UUID;
+
+@FeignClient(name = "wallet-client", url = "localhost:8085/api/v1/wallets")
+public interface WalletFeignClient {
+
+    @PutMapping("/payment/{id}")
+    ResponseEntity<WalletResponse> updateWallet(@PathVariable("id") Long id, Double price, UUID passengerId);
+}
---
+++ rides-service/src/main/java/com/java/akdev/ridesservice/exception/NotEnoughMoneyException.java
@@ -0,0 +1,8 @@
+package com.java.akdev.ridesservice.exception;
+
+public class NotEnoughMoneyException extends RuntimeException {
+
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+}
