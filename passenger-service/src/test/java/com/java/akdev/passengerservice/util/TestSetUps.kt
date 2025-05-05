4a07dedc76c2ac2b5d92b7c610603fdf773f68bf
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-05-05 11:10:33 +0300

    Added @UtilityClass annotation and included lombok in build.gradle and fixed kotlin dependency

--- passenger-service/src/test/java/com/java/akdev/passengerservice/util/TestSetUps.kt
+++ passenger-service/src/test/java/com/java/akdev/passengerservice/util/TestSetUps.kt
@@ -3,17 +3,19 @@ package com.java.akdev.passengerservice.util
 import com.java.akdev.passengerservice.dto.PassengerCreateDto
 import com.java.akdev.passengerservice.dto.PassengerReadDto
 import com.java.akdev.passengerservice.entity.Passenger
+import lombok.experimental.UtilityClass
 import java.util.*
 
+@UtilityClass
 class TestSetUps {
 
     companion object {
         val id: UUID = UUID.randomUUID()
-        const val NAME: String = "Anton"
-        const val LAST_NAME: String = "Kazlouski"
-        const val EMAIL: String = "anton@gmail.com"
-        const val PHONE_NUMBER: String = "1234567890"
-        const val PASSWORD: String = "123"
+        private const val NAME: String = "Anton"
+        private const val LAST_NAME: String = "Kazlouski"
+        private const val EMAIL: String = "anton@gmail.com"
+        private const val PHONE_NUMBER: String = "1234567890"
+        private const val PASSWORD: String = "123"
 
         fun getPassenger(): Passenger {
             return Passenger.builder()
