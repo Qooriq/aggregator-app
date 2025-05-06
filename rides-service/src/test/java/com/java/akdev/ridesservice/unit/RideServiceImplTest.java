98e146d8d2c6b2fa6048142a730001b9e60d9f6c
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-05-07 02:11:54 +0300

    unit tests for ride service

---
+++ rides-service/src/test/java/com/java/akdev/ridesservice/unit/RideServiceImplTest.java
@@ -0,0 +1,139 @@
+package com.java.akdev.ridesservice.unit;
+
+import com.java.akdev.ridesservice.dto.RideCreateDto;
+import com.java.akdev.ridesservice.dto.RideReadDto;
+import com.java.akdev.ridesservice.dto.RideUpdateDto;
+import com.java.akdev.ridesservice.entity.Ride;
+import com.java.akdev.ridesservice.exception.RideNotFoundException;
+import com.java.akdev.ridesservice.mapper.RideMapper;
+import com.java.akdev.ridesservice.repository.RideRepository;
+import com.java.akdev.ridesservice.service.impl.RideServiceImpl;
+import com.java.akdev.ridesservice.util.TestSetUps;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RideServiceImplTest {
+
+    @Mock
+    private RideMapper rideMapper;
+    @Mock
+    private RideRepository rideRepository;
+    @InjectMocks
+    private RideServiceImpl reviewService;
+
+    private Ride ride;
+    private Ride updateRide;
+    private RideCreateDto rideCreateDto;
+    private RideReadDto rideUpdateDto;
+    private RideUpdateDto rideForUpdateDto;
+    private RideReadDto rideReadDto;
+    private Long id;
+
+    @BeforeEach
+    void setUp() {
+        ride = TestSetUps.getRide();
+        id = TestSetUps.id;
+        rideCreateDto = TestSetUps.getCreateDto();
+        rideReadDto = TestSetUps.getReadDto();
+        rideUpdateDto = TestSetUps.getUpdateDto();
+        updateRide = TestSetUps.getUpdateReview();
+        rideForUpdateDto = TestSetUps.getRideUpdateDto();
+    }
+
+    @Test
+    @DisplayName("find ride by id")
+    void findDriverById() {
+
+        when(rideRepository.findById(id))
+                .thenReturn(Optional.of(ride));
+        when(rideMapper.toRideReadDto(ride))
+                .thenReturn(rideReadDto);
+
+        var rev = reviewService.findById(id);
+
+        assertThat(rev)
+                .isEqualTo(rideReadDto);
+
+        verify(rideMapper).toRideReadDto(ride);
+        verify(rideRepository).findById(id);
+    }
+
+    @Test
+    @DisplayName("driver not found exception")
+    void findDriverNotFoundById() {
+
+        when(rideRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() ->
+                reviewService.findById(id)
+        ).isInstanceOf(RideNotFoundException.class);
+
+        verify(rideRepository).findById(id);
+    }
+
+    @Test
+    @DisplayName("create review")
+    void createDriver() {
+        when(rideRepository.save(ride))
+                .thenReturn(ride);
+        when(rideMapper.toRide(rideCreateDto))
+                .thenReturn(ride);
+        when(rideMapper.toRideReadDto(ride))
+                .thenReturn(rideReadDto);
+
+        var rid = reviewService.create(rideCreateDto);
+
+        assertThat(rid)
+                .isEqualTo(rideReadDto);
+
+        verify(rideRepository).save(ride);
+        verify(rideMapper).toRide(rideCreateDto);
+        verify(rideMapper).toRideReadDto(ride);
+    }
+
+    @Test
+    @DisplayName("update driver by id")
+    void update() {
+        when(rideRepository.findById(id))
+                .thenReturn(Optional.of(ride));
+        when(rideRepository.save(ride))
+                .thenReturn(updateRide);
+        when(rideMapper.toRideReadDto(updateRide))
+                .thenReturn(rideUpdateDto);
+        when(rideMapper.updateRide(ride, rideForUpdateDto))
+                .thenReturn(updateRide);
+
+        var ridDto = reviewService.update(id, rideForUpdateDto);
+
+        assertThat(ridDto)
+                .isEqualTo(rideUpdateDto);
+
+        verify(rideRepository).findById(id);
+        verify(rideRepository).save(ride);
+        verify(rideMapper).toRideReadDto(updateRide);
+        verify(rideMapper).updateRide(ride, rideForUpdateDto);
+    }
+
+    @Test
+    @DisplayName("delete driver by id")
+    void deleteDriver() {
+        doNothing().when(rideRepository).deleteById(id);
+
+        reviewService.delete(id);
+
+        verify(rideRepository).deleteById(id);
+    }
+}
