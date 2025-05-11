0736083817eafd9d5421c1d84b5f021fb0b3d694
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-05-12 02:10:50 +0300

    fixed error with uploading configs in tests

--- review-service/src/main/java/com/java/akdev/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/com/java/akdev/reviewservice/ReviewServiceApplication.java
@@ -1,17 +1,13 @@
 package com.java.akdev.reviewservice;
 
 import com.java.akdev.reviewservice.config.AppConfiguration;
-import com.java.akdev.reviewservice.config.ArtemisConfiguration;
-import com.java.akdev.reviewservice.config.JpaTransactionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@Import({JpaTransactionConfiguration.class, ArtemisConfiguration.class})
 @EnableConfigurationProperties(
         {
                 AppConfiguration.class
