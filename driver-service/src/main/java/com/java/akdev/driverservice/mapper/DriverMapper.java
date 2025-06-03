d5fdc1423b872aa69069aa51d252f15c5d6bdc43
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-06-03 20:56:32 +0300

    added metrics for all services, included them through eureka

--- driver-service/src/main/java/com/java/akdev/driverservice/mapper/DriverMapper.java
+++ driver-service/src/main/java/com/java/akdev/driverservice/mapper/DriverMapper.java
@@ -1,13 +1,13 @@
 package com.java.akdev.driverservice.mapper;
 
 import com.java.akdev.commonmodels.dto.UserResponse;
-import com.java.akdev.driverservice.configuration.MapperConfiguration;
+import com.java.akdev.driverservice.configuration.MapperConfigurationn;
 import com.java.akdev.driverservice.dto.DriverCreateDto;
 import com.java.akdev.driverservice.entity.Driver;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(config = MapperConfiguration.class)
+@Mapper(config = MapperConfigurationn.class)
 public interface DriverMapper {
 
     UserResponse toUserResponse(Driver driver);
