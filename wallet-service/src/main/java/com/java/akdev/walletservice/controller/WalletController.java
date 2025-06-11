41456687359b57ca5713ed04adbe94793bcd50fb
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-06-12 01:14:39 +0300

    added trace id and span id for all services

--- wallet-service/src/main/java/com/java/akdev/walletservice/controller/WalletController.java
+++ wallet-service/src/main/java/com/java/akdev/walletservice/controller/WalletController.java
@@ -8,6 +8,7 @@ import com.java.akdev.walletservice.enumeration.SortField;
 import com.java.akdev.walletservice.service.WalletService;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api/v1/wallets")
 @RequiredArgsConstructor
+@Slf4j
 public class WalletController {
 
     private final WalletService walletService;
@@ -38,6 +40,7 @@ public class WalletController {
 
     @PostMapping
     public ResponseEntity<WalletReadDto> create(@RequestBody WalletCreateDto walletCreateDto) {
+        log.info("Create wallet with passenger id: {}", walletCreateDto.userId());
         return ResponseEntity.status(201)
                 .body(walletService.createWallet(walletCreateDto));
     }
@@ -50,7 +53,7 @@ public class WalletController {
     }
 
     @PutMapping("/payment/{id}")
-    ResponseEntity<WalletResponse> updateWallet(@PathVariable("id") UUID userId, Double price){
+    ResponseEntity<WalletResponse> updateWallet(@PathVariable("id") UUID userId, Double price) {
         return ResponseEntity.status(200)
                 .body(walletService.payment(userId, price));
     }
