64dbeb378db8f49b1ae68d13d5d3e6708ba07059
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-06-13 16:15:27 +0300

    removed unnecessary test and mocked jms

--- review-service/src/test/java/com/java/akdev/reviewservice/IntegrationTestBase.java
+++ review-service/src/test/java/com/java/akdev/reviewservice/IntegrationTestBase.java
@@ -9,15 +9,13 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.DirectoryResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -30,10 +28,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "eureka.client.enabled=false",
-        "spring.cloud.discovery.enabled=false"
-})
 @AutoConfigureMockMvc
 @Transactional
 @Rollback
@@ -42,6 +36,8 @@ import java.sql.SQLException;
 public class IntegrationTestBase {
 
     public static PostgreSQLContainer<?> POSTGRES;
+    @MockitoBean
+    public JmsTransactionManager jmsTransactionManager;
 
     static {
         POSTGRES = new PostgreSQLContainer<>("postgres:16")
