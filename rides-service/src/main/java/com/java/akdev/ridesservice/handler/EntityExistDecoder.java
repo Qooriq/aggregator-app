03aac8d336652b9c4d59ea879567f29f858173ec
Kozlovskiy Anton <toni.kozlovskiy.04@mail.ru>
2025-06-19 03:32:43 +0300

    feign new status code

--- rides-service/src/main/java/com/java/akdev/ridesservice/handler/EntityExistDecoder.java
+++ rides-service/src/main/java/com/java/akdev/ridesservice/handler/EntityExistDecoder.java
@@ -11,6 +11,8 @@ public class EntityExistDecoder implements ErrorDecoder {
         switch (response.status()) {
             case 404:
                 return new EntityNotFoundException("RideController.entity.notFound");
+            case 403:
+                return new RuntimeException("Forbidden");
             default:
                 return new RuntimeException("RideController.api.error");
         }
